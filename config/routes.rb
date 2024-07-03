97d50ba9c53a2156c32620fcf7763e0c4a6f7c17
IsCathal <mccabemcathal@gmail.com>
2024-07-03 06:37:56 +0100

    Added debug page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root 'ai_text_sessions#index'
+  #always plural here
+  resources :debugs,  except: [:destroy]
+
+
 
   # Defines the root path route ("/")
   # root "posts#index"
